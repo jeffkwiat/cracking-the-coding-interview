40d80ecc05b5b7048ad4fd255ab1ecf04030f93c
Jeff Kwiat <j@jeffkwiat.com>
2017-03-01 11:12:39 -0800

    removed old directories

--- data-structures/arrays_left_rotation.py
+++
@@ -1,9 +0,0 @@
-# Find all permutations of s in b.
-def array_left_rotation(a, n, k):
-    return a[k:] + a[:k]
-
-if __name__ == '__main__':
-    n, k = map(int, input().strip().split(' '))
-    a = list(map(int, input().strip().split(' ')))
-    answer = array_left_rotation(a, n, k)
-    print(*answer, sep='w ')
